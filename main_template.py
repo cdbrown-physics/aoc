fd03da6b59e258b8434303d6d84cef07c0c61d01
Cody Brown <cody2093@yahoo.com>
2024-12-03 21:19:38 -0700

    Adding 04 and main template

---
+++ main_template.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import logging
+import argparse
+
+def PartOne(file_path: str):
+    pass
+    
+def PartTwo(file_path: str):
+    pass
+    
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug", help="Debug level", default=20, type=int)
+    parser.add_argument("-p", "--path", help="Path to the input file we want to use", type=str)
+    args = parser.parse_args()
+    logging.basicConfig(level=args.debug)
+    PartOne(args.path)
+    PartTwo(args.path)
+    
+if __name__ == "__main__":
+    main()
