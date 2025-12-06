d3f39a6c80e2abdbdfbf65f3e4cc6d4b40991d2e
cody <cody2093@yahoo.com>
2025-12-05 21:55:42 -0700

    Done with part 1

--- 2025/day03/src/main.rs
+++ 2025/day03/src/main.rs
@@ -23,44 +23,69 @@ fn read_lines(filename: &str) -> Result<Vec<String>>
     Ok(lines)
 }
 
-fn get_tens_num_str(line: &String) -> Result<u32> {
-    let mut max_tens_int = 0;
-    let mut max_tens_index = -1;
+fn get_tens_num_index(line: &String) -> Result<usize> {
+    let mut max_tens_int: u32 = 0;
+    let mut max_tens_index: usize = 0;
     // Want to return the index of the value that corresponds to the character of the string.
     for i in 0..line.len()-1 {
         let c = line.as_bytes()[i] as char;
-        let possible_tens = c.to_diggit(10);
-        if possible_tens > max_tens { 
+        let possible_tens = c.to_digit(10).context("Failed to convert to digit")?;
+        if possible_tens > max_tens_int { 
             max_tens_int = possible_tens;
             max_tens_index = i;
         }
     }
-    Ok(max_tens_char)
+    Ok(max_tens_index)
 }
 
-fn find_joltage(line: String) -> Result<String> {
-    let tens_num_str = get_tens_num_str(line).context("Failed to find tens place number");
-    Ok("")
-}
-
-fn part_one(lines: &Vec<String>) -> Result<i32>
-{
-    for line in lines {
-        let joltage = find_joltage(&line);
+fn get_ones_num_index(line: &String, tens_index: usize) -> Result<usize> {
+    let mut max_ones_int: u32 = 0;
+    let mut max_ones_index: usize = 0;
+    // Want to return the index of the value that corresponds to the character of the string.
+    for i in (tens_index+1)..line.len() {
+        let c = line.as_bytes()[i] as char;
+        let possible_ones = c.to_digit(10).context("Failed to convert to digit")?;
+        if possible_ones > max_ones_int { 
+            max_ones_int = possible_ones;
+            max_ones_index = i;
+        }
     }
-    Ok()
+    Ok(max_ones_index)
+}
+
+fn find_joltage(line: &String) -> Result<String> {
+    let tens_num_index: usize = get_tens_num_index(&line).context("Failed to find tens place number")?;
+    let ones_num_index: usize = get_ones_num_index(&line, tens_num_index).context("Failed to find tens place number")?;
+    debug!("Indexes found tens: {tens_num_index} ones index: {ones_num_index}");
+    let tens_char: char = line.as_bytes()[tens_num_index] as char;
+    let ones_char = line.as_bytes()[ones_num_index] as char;
+    let joltage = format!("{tens_char}{ones_char}");
+    debug!("Number used is {joltage}");
+    Ok(joltage)
+}
+
+fn part_one(lines: &Vec<String>) -> Result<u32>
+{
+    let mut total_joltage = 0;
+    for line in lines {
+        debug!("Looking at line: {line}");
+        let joltage = find_joltage(&line)?;
+        let num_joltage: u32 = joltage.parse::<u32>()?;
+        total_joltage += num_joltage;
+    }
+    Ok(total_joltage)
 }
 
 fn part_two(lines: &Vec<String>) -> Result<i32>
 {
-    Ok()
+    Ok(0)
 }
 
 fn main() -> Result<()>
 {
     env_logger::init();
     info!("Starting program");
-    let lines = read_lines("test_data.txt");
+    let lines = read_lines("data.txt")?;
     let part_one_answer = part_one(&lines)?;
     println!("Answer to part one: {part_one_answer}");
     let part_two_answer = part_two(&lines)?;
