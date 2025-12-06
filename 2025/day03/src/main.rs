a151082993214a74c8a0e14f8d5c9f4b0c13e1dd
Cody Brown <cody2093@yahoo.com>
2025-12-05 20:53:20 -0700

    Starting day 3

---
+++ 2025/day03/src/main.rs
@@ -0,0 +1,69 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+use anyhow::{Context, Result};
+use log::{debug, info};
+use env_logger;
+use regex::Regex;
+
+fn read_lines(filename: &str) -> Result<Vec<String>>
+{
+    let path = Path::new(filename);
+    let file = File::open(&path)?;
+
+    let reader = io::BufReader::new(file);
+    let mut lines = Vec::new();
+
+    for line in reader.lines()
+    {
+        let line_content = line?;
+        lines.push(line_content);
+    }
+    Ok(lines)
+}
+
+fn get_tens_num_str(line: &String) -> Result<u32> {
+    let mut max_tens_int = 0;
+    let mut max_tens_index = -1;
+    // Want to return the index of the value that corresponds to the character of the string.
+    for i in 0..line.len()-1 {
+        let c = line.as_bytes()[i] as char;
+        let possible_tens = c.to_diggit(10);
+        if possible_tens > max_tens { 
+            max_tens_int = possible_tens;
+            max_tens_index = i;
+        }
+    }
+    Ok(max_tens_char)
+}
+
+fn find_joltage(line: String) -> Result<String> {
+    let tens_num_str = get_tens_num_str(line).context("Failed to find tens place number");
+    Ok("")
+}
+
+fn part_one(lines: &Vec<String>) -> Result<i32>
+{
+    for line in lines {
+        let joltage = find_joltage(&line);
+    }
+    Ok()
+}
+
+fn part_two(lines: &Vec<String>) -> Result<i32>
+{
+    Ok()
+}
+
+fn main() -> Result<()>
+{
+    env_logger::init();
+    info!("Starting program");
+    let lines = read_lines("test_data.txt");
+    let part_one_answer = part_one(&lines)?;
+    println!("Answer to part one: {part_one_answer}");
+    let part_two_answer = part_two(&lines)?;
+    println!("Answer to part two: {part_two_answer}");
+    Ok(())
+}
