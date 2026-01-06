9b264e8c5eb234d37a49b969e12fe74f4960be70
cody <cody2093@yahoo.com>
2026-01-06 07:58:29 -0700

    Working on day 8

--- 2025/day08/src/main.rs
+++ 2025/day08/src/main.rs
@@ -11,22 +11,55 @@ struct Args{
     #[arg(long)]
     path: PathBuf,
 }
-fn read_lines(filename: &Path) -> Result<Vec<String>> {
-    let file = File::open(filename).context("Failed to open the file")?;
+
+// Junction box struct
+#[derive(Debug)]
+
+struct JunctionBox {
+    x: u32,
+    y: u32,
+    z: u32,
+    index: u32
+}
+
+#[derive(Debug)]
+struct Circuit {
+    junction_boxs: Vec<u32>
+}
+
+fn read_lines(filename: &Path) -> Result<Vec<Vec<u32>>> {
+    /*For this problem I want to read in the file and store each line as a 
+    tuple of numbers X,Y,Z and then we can use those values for each 'box' */
+    let path = Path::new(filename);
+    let file = File::open(path)?;
+
     let reader = BufReader::new(file);
+    let mut lines = Vec::new();
 
     for line in reader.lines() {
-        let line_content = line?;
+        let line_content: Vec<u32> = line?.split(',').map(|s| s.trim().parse::<u32>().unwrap()).collect();
+        debug!("Line contents are {line_content:?}");
         lines.push(line_content);
     }
     Ok(lines)
 }
 
-fn part_one(lines: &Vec<String>) -> Result<u32>= {
+fn part_one(lines: &[Vec<u32>]) -> Result<u32> {
+    /*Planned steps
+    1.  */
+    println!("{lines:?}");
+    let mut junction_boxes: Vec<JunctionBox> = Vec::new();
+    let mut index: u32 = 0;
+    for line in lines {
+        let jb = JunctionBox {x: line[0], y: line[1], z: line[2], index};
+        index += 1;
+        junction_boxes.push(jb);
+    }
+    println!("{junction_boxes:?}");
     Ok(0)
 }
 
-fn part_two(lines: &Vec<String>) -> Result<u32> {
+fn part_two(lines: &[Vec<u32>]) -> Result<u32> {
     Ok(0)
 }
 
